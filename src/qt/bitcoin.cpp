7d1f13c01990541ae071b1dd967c9c51100bb08c
noise23 <admin@acidpool.com>
2015-06-10 00:12:47 +0300

    Change splash message text color

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -88,7 +88,7 @@ static void InitMessage(const std::string &message)
 {
     if(splashref)
     {
-		splashref->showMessage("Personal Vault "  + QString::fromStdString(FormatFullVersion().c_str()) + " : " + QString::fromStdString(message), Qt::AlignBottom|Qt::AlignHCenter, QColor(194,145,114));
+		splashref->showMessage("Personal Vault "  + QString::fromStdString(FormatFullVersion().c_str()) + " : " + QString::fromStdString(message), Qt::AlignBottom|Qt::AlignHCenter, QColor(213,241,255));
         QApplication::instance()->processEvents();
     }
 }
