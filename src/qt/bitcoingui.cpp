835c335f182067c6fe4a7157a6dbabe661587040
noise23 <admin@acidpool.com>
2015-06-12 21:31:18 +0300

    QToolBar text under icon

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -363,7 +363,7 @@ void BitcoinGUI::createMenuBar()
 void BitcoinGUI::createToolBars()
 {
     QToolBar *toolbar = addToolBar(tr("Tabs toolbar"));
-    toolbar->setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
+    toolbar->setToolButtonStyle(Qt::ToolButtonTextUnderIcon);
     toolbar->addAction(overviewAction);
     toolbar->addAction(sendCoinsAction);
     toolbar->addAction(receiveCoinsAction);
@@ -374,7 +374,7 @@ void BitcoinGUI::createToolBars()
 #endif
 
     QToolBar *toolbar2 = addToolBar(tr("Actions toolbar"));
-    toolbar2->setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
+    toolbar2->setToolButtonStyle(Qt::ToolButtonTextUnderIcon);
     toolbar2->addAction(blockAction);
     toolbar2->addAction(exportAction);
 
