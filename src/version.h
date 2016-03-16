c3f538da76868665f5427ea3e88962d975c8cdb1
noise23 <admin@acidpool.com>
2016-03-17 01:20:52 +0200

    v2.2 protocol 73023

--- src/version.h
+++ src/version.h
@@ -13,7 +13,7 @@
 
 // These need to be macro's, as version.cpp's voodoo requires it
 #define CLIENT_VERSION_MAJOR       2
-#define CLIENT_VERSION_MINOR       1
+#define CLIENT_VERSION_MINOR       2
 #define CLIENT_VERSION_REVISION    0
 #define CLIENT_VERSION_BUILD       0
 
@@ -31,21 +31,21 @@ extern const std::string CLIENT_DATE;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 73002; 
+static const int PROTOCOL_VERSION = 73023; 
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 73002; 
+static const int MIN_PEER_PROTO_VERSION = 73023; 
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
-static const int CADDR_TIME_VERSION = 73002;
+static const int CADDR_TIME_VERSION = 73023;
 
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 32000;
-static const int NOBLKS_VERSION_END = 73001;
+static const int NOBLKS_VERSION_END = 73022;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
