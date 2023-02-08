f838cd64ea6c754c4c3a91ce71d2034e99b375b3
noise23 <noise23@users.noreply.github.com>
2023-02-08 04:23:16 +0200

    Remove getwork and getworkex RPC.

--- src/bitcoinrpc.h
+++ src/bitcoinrpc.h
@@ -158,7 +158,6 @@ extern json_spirit::Value getgenerate(const json_spirit::Array& params, bool fHe
 extern json_spirit::Value setgenerate(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value gethashespersec(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value getmininginfo(const json_spirit::Array& params, bool fHelp);
-extern json_spirit::Value getwork(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value getstaking(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value setstaking(const json_spirit::Array& params, bool fHelp);
 
